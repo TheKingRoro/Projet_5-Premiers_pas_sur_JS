cc10bae0a7b7402f1538c96ac023f7d808464376
Aurélie <126612267+TheKingRoro@users.noreply.github.com>
2023-06-08 17:28:21 +0200

    Merci PinPin ^^

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,41 @@
-/*const slides = [
-	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-
-]*/
-
 const slidestry = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"];
-
 
 const slidestrytagline = ["Impressions tous formats <span>en boutique et en ligne</span>",
 	"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	"Autocollants <span>avec découpe laser sur mesure</span>"]
+	"Autocollants <span>avec découpe laser sur mesure</span>"];
 
 window.addEventListener(
 	"DOMContentLoaded", (event) => {
-		const slidedot = document.querySelectorAll(".dot")
-		console.log(slidedot)
-});
+		
+		// pointer l'id de la div "dots-line"
+		var p = document.getElementById("dots-line");
+		
+		// boucle de création des dots
+		for(var i=1; i<=(slidestry.length - 1); i++) {
+			var e= document.createElement("div");
+			e.id = "dot"+i;
+			e.className = "dot";
+			p.appendChild(e);
+		}
+		
+		const slidedot = document.querySelector(".dot");
+		console.log(slidedot);
+}
+, false);
 
 let i = 0;
-let y = 3;
+let y = (slidestry.length - 1);
 
 function ArrowSlide(sens) {
 	y = i;
 	i = i + sens;
-	if (i > 3)
+	if (i > (slidestry.length - 1))
 		i = 0;
 	if (i < 0)
-		i = 3;
+		i = (slidestry.length - 1);
 	document.getElementById("image").src = "./assets/images/slideshow/" + slidestry[i];
 	document.getElementById("tagLine").innerHTML = slidestrytagline[i];
 	document.getElementById("dot"+y).classList.remove("dot_selected");
 	document.getElementById("dot"+i).classList.add("dot_selected");
-}
-
-
-
-
+};
