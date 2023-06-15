83a4f9e1dcf7bca4ec08ac6d505f165dffdd834a
TheKingRoro <peyroutet.aurelie@gmail.com>
2023-06-15 19:08:40 +0200

    Correction, console ok

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,40 @@
-const slidestry = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"];
+//Tableaux images et taglines
+const slidesImage = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"];
 
-const slidestrytagline = ["Impressions tous formats <span>en boutique et en ligne</span>",
-	"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	"Autocollants <span>avec découpe laser sur mesure</span>"];
+const slidesTagline = [
+  "Impressions tous formats <span>en boutique et en ligne</span>",
+  "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  "Autocollants <span>avec découpe laser sur mesure</span>",
+];
 
-window.addEventListener(
-	"DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Sélectionner la div parent #dots-line
+  let dotsLine = document.getElementById("dots-line");
 
-		// pointer l'id de la div "dots-line"
-		let p = document.getElementById("dots-line");
+  // Boucle de création des child .dot
+  for (let z = 1; z <= slidesImage.length - 1; z++) {
+    let newDot = document.createElement("div");
+    newDot.id = "dot" + z;
+    newDot.className = "dot";
+    dotsLine.appendChild(newDot);
+  }
+});
 
-		// boucle de création des dots
-		for (let z = 1; z <= (slidestry.length - 1); z++) {
-			let e = document.createElement("div");
-			e.id = "dot" + z;
-			e.className = "dot";
-			p.appendChild(e);
-		}
-	}
-);
-
+//Variables d'initialisation
 let i = 0;
-let y = (slidestry.length - 1);
+let y = slidesImage.length - 1;
 
-function ArrowSlide(sens) {
-	y = i;
-	i = i + sens;
-	if (i > (slidestry.length - 1))
-		i = 0;
-	if (i < 0)
-		i = (slidestry.length - 1);
-	document.getElementById("image").src = "./assets/images/slideshow/" + slidestry[i];
-	document.getElementById("tagLine").innerHTML = slidestrytagline[i];
-	document.getElementById("dot" + y).classList.remove("dot_selected");
-	document.getElementById("dot" + i).classList.add("dot_selected");
-};
+function arrowSlide(sens) {
+  y = i;
+  i = i + sens;
+  if (i > slidesImage.length - 1) i = 0;
+  if (i < 0) i = slidesImage.length - 1;
+  document.getElementById("image").src =
+    "./assets/images/slideshow/" + slidesImage[i];
+  document.getElementById("tagLine").innerHTML = slidesTagline[i];
+  document.getElementById("dot" + y).classList.remove("dot_selected");
+  document.getElementById("dot" + i).classList.add("dot_selected");
+}
+
+console.log(arrowSlide);
